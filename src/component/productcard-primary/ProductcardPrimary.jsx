0fe63666e42ee55d1c13fb48e60d718704dcf6c6
Jubayer Juhan <davidjuhan23@gmail.com>
2022-01-29 21:35:15 +0600

    Product card updated

--- src/component/productcard-primary/ProductcardPrimary.jsx
+++ src/component/productcard-primary/ProductcardPrimary.jsx
@@ -3,14 +3,14 @@ import "./productcardprimary.css";
 
 const ProductcardPrimary = ({ product }) => {
   return (
-    <div className="productCard">
+    <div className="productCard productCard">
       <div className="productCard__image">
         <img src={product.images[0].url} alt={product.name} />
       </div>
       <div className="productCard__name">
         <h3>{product.name}</h3>
       </div>
-      <div className="productCard__price">
+      <div iv className="productCard__price">
         <p>{`৳ ${product.price}`}</p>
       </div>
     </div>
